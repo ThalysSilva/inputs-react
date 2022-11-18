9d7de14849798236f85d15dee0cc6431c6e7495b
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-11-18 02:50:08 -0300

    fix: adjusts in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,51 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('./src/styles/Theme/colors.js');
+const colors = require("./src/styles/Theme/colors.js");
 module.exports = {
-  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     screen: {
-      sm: '30em',
-      md: '48em',
-      lg: '62em',
-      xl: '80em',
-      '2xl': '96em',
+      sm: "30em",
+      md: "48em",
+      lg: "62em",
+      xl: "80em",
+      "2xl": "96em",
     },
     extend: {
       colors: colors,
       transformOrigin: {
-        'top-center': 'top center',
-        'bottom-center': 'bottom center',
+        "top-center": "top center",
+        "bottom-center": "bottom center",
       },
       transitionProperty: {
-        height: 'height',
+        height: "height",
       },
       keyframes: {
         heightModalAnimation: {
-          '0%': {
-            maxHeight: '0px',
+          "0%": {
+            maxHeight: "0px",
 
             opacity: 0,
           },
 
-          '100%': {
-            maxHeight: '1080px',
+          "100%": {
+            maxHeight: "1080px",
 
             opacity: 1,
           },
         },
         fadeInAnimation: {
-          '0%': { opacity: 0 },
+          "0%": { opacity: 0 },
 
-          '100%': { opacity: 1 },
+          "100%": { opacity: 1 },
         },
       },
 
       animation: {
-        heightModalAnimation: 'heightModalAnimation 500ms ease-in-out',
-        fadeInAnimation: ' fadeInAnimation 200ms ease-in-out',
+        heightModalAnimation: "heightModalAnimation 500ms ease-in-out",
+        fadeInAnimation: " fadeInAnimation 200ms ease-in-out",
       },
     },
   },
